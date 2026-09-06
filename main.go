2e95ec64ff285ca704c7316f969d14abae07257f
nayab rehmat <mnr282001@gmail.com>
2026-09-06 15:40:15 -0500

    refactor: reorganize imports and improve formatting in main.go

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "os"
-
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 //         docker run image <cmd> <params>
 // go run main.go run       <cmd> <params>
@@ -18,6 +20,13 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
 }
 
 func must(err error) {
